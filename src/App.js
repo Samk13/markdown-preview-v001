0f582aaf400a20ec1828d3a48ad8f199a8a3f24b
sam_arbid <samarbid13@users.noreply.github.com>
2019-07-08 16:14:38 +0200

    'deploy'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import marked from 'marked'
+import hljs from 'highlight.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from './components/NavBar'
+import { Button, FormGroup, Label, Input, Jumbotron, Container, Row, Col } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const initialMarkdown = `
+### Headers
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+### List 
+- list item one
+- list item two
+- list item three
+### links
+[My Github](https://github.com/Samk13)
+[My LinkedIn](https://www.linkedin.com/in/samarbid/)
+
+### Text Decorations
+*italic* and **bold**
+### Images 
+![](https://apprecs.org/ios/images/app-icons/256/96/343544214.jpg)
+
+### Blockquote
+> life will give you what ever you think of .
+### code 
+\` npm install create-react-app -g \`
+\`\`\`
+const x = 10 ;
+const y = 15;
+let func(x,y) => (x+y);
+console.log (func);
+\`\`\`
+`
+
+let renderer = new marked.Renderer()
+renderer.link = function (href, title, text) {
+  return `<a href=${href} target= '_blank'>${text}</a>`
+}
+marked.setOptions({
+  renderer: renderer,
+  breaks: true,
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value
+  }
+})
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdown: initialMarkdown
+    }
+  }
+  handelChange = e => this.setState({ markdown: e.target.value })
+
+
+  render() {
+    return (
+
+      <div>
+        <NavBar />
+        <Jumbotron fluid className='Jumbotron'>
+          <h1 className="display-3 text-center text-success ">🛰Markdown Preview 🛰</h1>
+          <p className="lead text-center text-warning">This is my markdown App from FCC Front End Libraries Project created by Sam Arbid </p>
+
+        </Jumbotron>
+        <Button color="primary" size="lg" block onClick={() => this.setState({ markdown: '' })}>ClearText</Button>
+        <br />
+
+        <Container>
+          <Row>
+
+
+            <Col>
+              <FormGroup>
+                <Label for="exampleText">Write your text here  <span role="img" aria-label="write your text here">✍</span></Label>
+                <Input className='left' style={{ height: 900 }} type="textarea" id='editor' name="text" value={this.state.markdown} onChange={this.handelChange} />
+              </FormGroup>
+            </Col>
+
+
+            <Col>
+              <p>MarkdownResult <span role="img" aria-label="look down">👇</span></p>
+              <div className='right'>
+                <div id='preview'
+                  dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }} />
+              </div>
+            </Col>
+          </Row>
+
+        </Container>
+
+      </div>
+
+    )
+  }
 }
 
 export default App;
